da0a354a5291f522bdd0d0b4f6b114aaa903b3dd
paul9 <159943188+Pavio6@users.noreply.github.com>
2026-04-22 10:43:16 +0500

    fix(db): добавить отслеживание версий для миграции 0006

--- wms/migrations/0006_skus_name_unique.up.sql
+++ wms/migrations/0006_skus_name_unique.up.sql
@@ -1,2 +1,10 @@
+BEGIN;
+
 ALTER TABLE wms_inventory.skus
-ADD CONSTRAINT skus_name_unique UNIQUE (name);
+  ADD CONSTRAINT skus_name_unique UNIQUE (name);
+
+INSERT INTO public.schema_migrations(version)
+VALUES (6)
+ON CONFLICT (version) DO NOTHING;
+
+COMMIT;
